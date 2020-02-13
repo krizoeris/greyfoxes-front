b3663c3b384cff113f3e1b61912ec81cc2e24d00
mathias <mathenderson@protonmail.com>
2020-02-13 14:58:23 +0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Banner from './components/Banner';
+import ProductSlide from './components/ProductSlide';
+import ProductCard from './components/ProductCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Navbar />
+      <Banner />
+      {/* <ProductSection> */}
+        <ProductCard/>
+      {/* </ProductSection> */}
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const Banner = () => {
+  return(
+    <div className="banner">
+
+{/* <!-- HOT DEAL SECTION --> */}
+		<div id="hot-deal" class="section">
+			{/* <!-- container --> */}
+			<div class="container">
+				{/* <!-- row --> */}
+				<div class="row">
+					<div class="col-md-12">
+						<div class="hot-deal">
+							<ul class="hot-deal-countdown">
+								<li>
+									<div>
+										<h3>02</h3>
+										<span>Days</span>
+									</div>
+								</li>
+								<li>
+									<div>
+										<h3>10</h3>
+										<span>Hours</span>
+									</div>
+								</li>
+								<li>
+									<div>
+										<h3>34</h3>
+										<span>Mins</span>
+									</div>
+								</li>
+								<li>
+									<div>
+										<h3>60</h3>
+										<span>Secs</span>
+									</div>
+								</li>
+							</ul>
+							<h2 className="text-banner-h1">hot deal this week</h2>
+							<p className="text-banner-p">New Collection Up to 50% OFF</p>
+							<a class="primary-btn cta-btn" href="#">Shop now</a>
+						</div>
+					</div>
+				</div>
+				{/* <!-- /row --> */}
+			</div>
+			{/* <!-- /container --> */}
+		</div>
+
+    </div>
+  )
+}
+
+export default Banner
